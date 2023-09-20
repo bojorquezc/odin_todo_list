2cf5da72024ca1208fa96abecd5c7c27831f1928
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-09-19 17:12:12 -0700

    Add create and delete methods

--- src/todo_creation.js
+++ src/todo_creation.js
@@ -2,26 +2,38 @@ const todoProjects = {
 };
 
 const todoItem = (taskName, taskDescription, dueDate, priority, project) => {
-    return { taskName, taskDescription, dueDate, priority, project }
-}
+    const projectExists = project in todoProjects;
 
-function pushToArray(todoItem) {
-    const arrayName = todoItem.project;
-    const projectExists = arrayName in todoProjects;
-    if (projectExists === true) {
-        todoProjects[`${arrayName}`].push(todoItem);
-    } else {
-        todoProjects[`${arrayName}`] = [];
-        todoProjects[`${arrayName}`].push(todoItem);
+    function pushToDo() {
+        if (projectExists === true) {
+            todoProjects[`${project}`].push(this);
+        } else {
+            todoProjects[`${project}`] = [];
+            todoProjects[`${project}`].push(this);
+        }
     }
+
+    function deleteToDo() {
+        if (projectExists === true) {
+            for (let index = 0; index < todoProjects[`${project}`].length; index+=1) {
+                if (todoProjects[`${project}`][index].taskName === taskName) {
+                    todoProjects[`${project}`].splice(index, 1);
+                }
+            }
+        }
+    }
+
+    return { taskName, taskDescription, dueDate, priority, project, pushToDo, deleteToDo }
 }
 
 
 const buyFood = todoItem('Buy Food', 'Go to the market and buy food', 'August 25 2023', 'medium', 'home');
-pushToArray(buyFood);
+buyFood.pushToDo()
 
 const doLaundry = todoItem('Do Laundry', 'Wash my clothes', 'August 27 2023', 'medium', 'gym');
-pushToArray(doLaundry);
+// pushToDo(doLaundry);
+doLaundry.pushToDo();
 
 const cookFood = todoItem('Cook Food', 'Cook tasty food', 'August 29 2023', 'medium', 'home');
-pushToArray(cookFood);
+// pushToDo(cookFood);
+cookFood.pushToDo();
