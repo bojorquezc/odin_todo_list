2368c724244182de7dacaeef0e60ce859ef1b0a7
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-10-19 00:50:00 -0700

    Start instructions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { saveLocalStorage, fetchLocalStorage } from './todo_manager';
 import { loadSampleContent } from './todo_sample_content';
 
 // fetchLocalStorage();
-loadSampleContent()
-displayTodo()
+// loadSampleContent()
+// displayTodo()
 allTaskButtonAddListener();
+
+
 
 // function startProject() {
 //     if (localStorage.length === 0) {
